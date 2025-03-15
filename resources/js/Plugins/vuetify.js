6c80d1359a3030a78bc12490f2774ab760d1b25b
dizlexic <dan@mooresolutions.io>
2025-03-15 03:18:24 -0500

    Add base light and dark themes to Vuetify configuration
    Introduced `BaseLightTheme` and `BaseDarkTheme` with customized colors and variables. Updated the Vuetify plugin to include these themes and set `BaseLightTheme` as the default. This improves theme flexibility and prepares for dark mode support.
    
    Signed-off-by: dizlexic <dan@mooresolutions.io>

--- resources/js/Plugins/vuetify.js
+++ resources/js/Plugins/vuetify.js
@@ -1,19 +1,31 @@
 // Vuetify
 import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import '@mdi/font/css/materialdesignicons.css'
 import {createVuetify} from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import {BaseLightTheme} from "@/Themes/BaseLightTheme.js";
+import {BaseDarkTheme} from "@/Themes/BaseDarkTheme.js";
 
 const conf = {
     components,
     directives,
+    theme: {
+        defaultTheme: "BaseLightTheme",
+        variations: {
+            colors: ['primary', 'secondary'],
+            lighten: 4,
+            darken: 4,
+        },
+        themes: {
+            BaseLightTheme,
+            BaseDarkTheme,
+        },
+    },
 }
+
 export const vuetify = createVuetify(conf)
 
-export const ssr = createVuetify({
-    ...conf,
-    ssr: true,
-})
+export const ssr = createVuetify(Object.assign({}, conf, {ssr: true}))
 
 export default vuetify
